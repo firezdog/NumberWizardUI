81bb4a8a6962e54d2f6fe18a71d654e585bbf461
Alex Leibowitz <firezdog@yahoo.com>
2018-09-19 22:59:39 -0500

    added scene transitions and layout for core game

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadNextScene()
+	{
+		int totalScenes = SceneManager.sceneCountInBuildSettings;
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		int nextSceneIndex = (currentSceneIndex + 1) % totalScenes;
+		SceneManager.LoadScene(nextSceneIndex);
+	}
+
+}
